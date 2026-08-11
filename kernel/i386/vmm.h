62591d6ffa6ab8c01e8f42c98bef2dcb823530a3
Antonio Lorenzo <antoniolornunes@gmail.com>
2026-08-11 18:40:22 +0000

    rewrite: rewrote the vmm with more features

--- kernel/i386/vmm.h
+++ kernel/i386/vmm.h
@@ -1,11 +1,25 @@
 #ifndef VMM_H
 #define VMM_H
 #include "../lib/types.h"
+
 #define PAGE_SIZE 4096
 #define PAGE_PRESENT 1
 #define PAGE_WRITE 2
-void map_page(uint32_t virt_addr, uint32_t phys_addr, uint32_t flags);
-void enable_paging(void);
-void load_page_directory(uint32_t phys_addr);
+#define PAGE_USER 4
+
+typedef struct page_directory {
+    uint32_t entries[1024];
+} page_directory_t;
+
+extern page_directory_t* current_pd;
+extern page_directory_t* kernel_pd;
+
 void init_paging(void);
+page_directory_t* vmm_create_address_space(void);
+void vmm_switch_to(page_directory_t* pd);
+void vmm_map_page(page_directory_t* pd, uint32_t virt, uint32_t phys, uint32_t flags);
+void vmm_unmap_page(page_directory_t* pd, uint32_t virt);
+uint32_t vmm_get_phys_addr(page_directory_t* pd, uint32_t virt);
+void vmm_clone_address_space(page_directory_t* src, page_directory_t* dst);
+
 #endif
